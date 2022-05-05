b912e9c89101342ad94062509e006fd9e3a1fda5
Quang Pham <quangpham.245@protonmail.com>
2022-05-05 14:18:15 +0700

    Add data-binding between HTML markup and Vue object

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,66 @@
 Vue.createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      name: "",
+      email: "",
+      phone: "",
+      content: "",
     };
   },
+  methods: {
+    onSubmit: function () {
+      console.info("Form submited");
+    },
+    onReset: function () {
+      this.name = this.email = this.phone = this.content = "";
+      console.info("Form reset");
+    },
+  },
+  watch: {
+    name: function (newValue, oldValue) {
+      console.info(`Name changed from ${oldValue} to ${newValue}`);
+    },
+    email: function (newValue, oldValue) {
+      console.info(`Email changed from ${oldValue} to ${newValue}`);
+    },
+    phone: function (newValue, oldValue) {
+      const sanitizedPhone = sanitizePhone(newValue);
+      this.phone = sanitizedPhone;
+      console.info(`Phone changed from ${oldValue} to ${sanitizedPhone}`);
+    },
+    content: function (newValue, oldValue) {
+      console.info(`Content changed from "${oldValue}" to "${newValue}"`);
+    },
+  },
 }).mount("#vm");
+
+/**
+ * Sanitize phone
+ * @param {string} phone phone to sanitize
+ * @returns {string} sannitized phone
+ */
+function sanitizePhone(phone) {
+  return truncatePhone(removeNonDigitChars(phone));
+}
+
+/**
+ * Remove all non-digit chars in phone
+ * @param {string} phone phone to be modified
+ * @returns {string} phone with non-digit chars removed
+ */
+function removeNonDigitChars(phone) {
+  return phone.replace(/\D{1}/, "");
+}
+
+/**
+ * Truncate phone if longer than 10 digits
+ * @param {string} phone phone to truncate
+ * @returns {string} 10-digit phone if it longer than 10 digits, otherwise return without any unmodification
+ */
+function truncatePhone(phone) {
+  if (phone.length > 10) {
+    return [...phone].splice(0, 10).join("");
+  } else {
+    return phone;
+  }
+}
